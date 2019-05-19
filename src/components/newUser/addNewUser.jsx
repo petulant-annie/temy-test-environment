c4489fc27d8234c049047f889ae82a9e06fc4b78
Anna Zakharenko <annazh@MBP-Anna.Dlink>
2019-05-19 14:32:26 +0300

    small fix

--- src/components/newUser/addNewUser.jsx
+++ src/components/newUser/addNewUser.jsx
@@ -3,16 +3,17 @@ import * as React from 'react';
 export default class NewUser extends React.Component {
   render(){
     return(
-      <div className="new-user">
-        <input type="text" className="name" placeholder="Name"/>
-        <input type="email" className="email" placeholder="Email"/>
-        <select name="country" id="country"></select>
-        <select name="state" id="state"></select>
-        <select name="city" id="city"></select>
-        <input type="number" className="phone" placeholder="Phone Number"/>
+      <form className="new-user">
+        <input type="text" className="name" placeholder="Name" required/>
+        <input type="email" className="email" placeholder="Email" required/>
+        <select name="country" id="country" required></select>
+        <select name="state" id="state" required></select>
+        <select name="city" id="city" required></select>
+        <input type="number" className="phone" placeholder="Phone Number" required/>
         <input type="text" className="adress" placeholder="Adress"/>
         <textarea name="about" id="about" cols="30" rows="10" maxLength="500" placeholder="About Me"></textarea>
-      </div>
+        <input type="submit"/>
+      </form>
     );
   }
 }
---
+++ src/components/userList/userList.jsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+export default class UserList extends React.Component {
+
+}
